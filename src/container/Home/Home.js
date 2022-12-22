cc84412a00ce013e470b209a0f92299905dca03b
Jai Gawande <gawande20jai@gmail.com>
2022-12-22 18:54:21 +0530

    4th push

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import "./home.css"
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div className="form__container">
+
+    <div className='form__container-input'>
+      <p>Name</p>
+      <input type='text'/>
+    </div>
+    <div className='form__container-input'>
+      <p>Contact no</p>
+      <input type='text'/>
+    </div>
+    <div className='form__container-input'>
+      <p>Address</p>
+      <input type='text'/>
+    </div>
+    <div className='form__container-input'>
+      <p>State</p>
+      <input type='text'/>
+    </div>
+    <div className='form__container-input'>
+      <p>City</p>
+      <input type='text'/>
+    </div>
+    <div className='form__container-input'>
+      <p>Pincode</p>
+      <input type='text'/>
+    </div>
+
+    <div className="form__container-submit-btn">
+      <button type='button'>Submit</button>
+    </div>
+
+    </div>
   )
 }
 
